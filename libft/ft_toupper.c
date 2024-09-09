466903706a571fd61b6ab41254cbfd07dfffd406
GAMAONSOVA <155147709+GAMAONSOVA@users.noreply.github.com>
2024-09-09 16:05:48 +0300

    Add files via upload

---
+++ libft/ft_toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int
+ft_toupper(int c)
+{
+    if(c >= 97 && c <= 122)
+        return (c-32);
+
+    else 
+        return (c);
+}
+
+int main()
+{
+    int a = 'G';
+    ft_toupper(a);
+    printf("%с", a);
+}
